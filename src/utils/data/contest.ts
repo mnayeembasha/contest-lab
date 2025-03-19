c70a0f4c31737c919065fe748009bc2eac9bbde9
Nayeem Basha <mnayeembasha@gmail.com>
2025-03-19 17:15:39 +0530

    Update contest.ts

--- src/utils/data/contest.ts
+++ src/utils/data/contest.ts
@@ -4,8 +4,8 @@ import { problems } from "./problems";
 export const mockContest: Contest = {
     contestId: "679e11660e4c800460d1351a",
     contestName: "Algo Hustle",
-    startTime: "2025-03-12T01:40:00Z",
-    duration: "20h",
+    startTime: "2025-03-19T01:40:00Z",
+    duration: "120h",
     isActive:true,
     problems:problems
 }
